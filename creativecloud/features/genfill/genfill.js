883aa3ba16308bfef77c44d71c4c4fc5134a7634
Drashti Modasara <drashti@Drashtis-MacBook-Pro.local>
2023-11-16 13:47:29 +0530

    removed redundant code

--- creativecloud/features/genfill/genfill.js
+++ creativecloud/features/genfill/genfill.js
@@ -2,54 +2,35 @@ import createEnticement from '../enticement/enticement.js';
 
 let autocycleInterval = undefined;
 let isImageClicked = false;
-
 let autocycleIndex = 0;
 
 function startAutocycle(interval, pics) {
   if (isImageClicked) return;
-  // setTimeout(startAutocycle, delay);
   autocycleInterval = setInterval(() => {
-    // showNextImage();
-
     autocycleIndex = handleTransition(autocycleIndex, pics);
     if (autocycleIndex === pics.length - 1) {
-      // stopAutocycle(autocycleInterval);
       clearInterval(autocycleInterval);
     }
   }, interval);
 }
 
 function handleTransition(index, pics) {
-  // event
+  console.log(index, pics[0].closest('div').classList);
   pics[index].style.display = 'none';
   const nextIndex = (index + 1) % pics.length;
   pics[nextIndex].style.display = 'block';
   return nextIndex;
 }
 
-function hideImages(imageArray) {
-  // imageArray[0].style.display = 'block'; //remove - by css
+function hideImages(imageArray) { //rename
   imageArray.forEach((picture, index) => {
-    // const imgElement = picture.querySelector('img');
     picture.querySelector('img')?.removeAttribute('loading');
-    // if (index !== 0) {
-    //   picture.style.display = 'none';
-    // }
-    picture.onclick = function click() {
+    picture.addEventListener("click", (e) => {
       isImageClicked = true;
       if (autocycleInterval) clearInterval(autocycleInterval);
       handleTransition(index, imageArray);
-    }; // addEventListener 
+    });
   });
-}
-
-function hideDetails(container) {
-  const enticement = container.firstElementChild.children[0];
-  const delay = container.firstElementChild.children[1];
-  enticement.classList.add('enticement-detail');
-  delay.classList.add('timer');
-
-  // remove these
 }
 
 function addEnticement(container) {
@@ -57,55 +38,67 @@ function addEnticement(container) {
   const enticementElement = createEnticement(enticementDetail);
   enticementElement.classList.add('enticement');
   container.insertBefore(enticementElement, container.firstElementChild);
+  // delete enticement p tag
+  // container.firstElementChild.children[0].remove();
 }
 
-function handleAutocycle(imageContainer, images, interval, delay) {  
-  let currentImageIndex = 0;
-  let autocycleInterval;
 
-  // function stopAutocycle() {
-  //   clearInterval(autocycleInterval);
-  // }
+function defineDeviceByScreenSize() {
+  const DESKTOP_SIZE = 1200;
+  const MOBILE_SIZE = 600;
+  const screenWidth = window.innerWidth;
+  if (screenWidth >= DESKTOP_SIZE) {
+    return 'desktop';
+  }
+  if (screenWidth <= MOBILE_SIZE) {
+    return 'mobile';
+  }
+  return 'tablet';
+}
 
-  // function showNextImage() {
-  //   images[currentImageIndex].style.display = 'none';
-  //   currentImageIndex += 1;
-  //   images[currentImageIndex].style.display = 'block';
-  //   if (currentImageIndex >= images.length - 1) {
-  //     stopAutocycle(autocycleInterval);
-  //   }
-  // }
-
-  
-
-  // imageContainer.addEventListener('click', () => {
-  //   stopAutocycle();
-  // });
-  // setTimeout(startAutocycle, delay);
+function removePTags(media) {
+  console.log(media.querySelectorAll('picture'));
+  const pics = media.querySelectorAll('picture');
+  pics.forEach((pic) => {
+    if (pic.closest('p')) {
+      pic.closest('p').remove();
+    }
+    media.appendChild(pic);
+  });
 }
 
 export default function decorateGenfill(el) {
   const interactiveContainer = el.querySelector('.interactive-container');
-  hideDetails(interactiveContainer); // to be removed
-  const timer = interactiveContainer.querySelector('.media .timer');
-  const intervalTimer = timer ? timer.innerText.split('|')[0] : 2000;
-  const delayTimer = timer?.length > 1 ? timer.innerText.split('|')[1] : 1000;
+
+  const enticement = interactiveContainer.firstElementChild.children[0];
+  enticement.classList.add('enticement-detail');
   
+  const timer = interactiveContainer.firstElementChild.children[1];
+  const [intervalTime = 2000, delayTime = 1000] = timer?.innerText.split('|');
+
   // get time & remove p tag
   // set default, return default
+  addEnticement(interactiveContainer);
+
+  enticement.remove();
+  timer.remove();
+
+  console.log('kkk', interactiveContainer.children[1]);
+
   const viewports = ['mobile', 'tablet', 'desktop'];
   const mediaElements = interactiveContainer.querySelectorAll('.media');
   viewports.forEach((viewport, viewportIndex) => {
     const media = mediaElements[viewportIndex]? mediaElements[viewportIndex] : mediaElements[viewportIndex-1];
     media.classList.add(`${viewport}-media`);
-    // console.log(mediaElements);
+    if (viewport === 'mobile') {
+      removePTags(media);
+    }
     const pictures = media.querySelectorAll('picture:not(.arrow-svg)');
     hideImages(pictures);
-    // showImages(media, pictures, intervalTimer, delayTimer);
-    setTimeout(() => {
-      startAutocycle(intervalTimer, pictures);
-    }, delayTimer);
-    // startAutocycle(intervalTimer, pictures)
+    if (defineDeviceByScreenSize() == viewport) {
+      setTimeout(() => {
+        startAutocycle(intervalTime, pictures);
+      }, delayTime);
+    }
   });
-  addEnticement(interactiveContainer);
 }
