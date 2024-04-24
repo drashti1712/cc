2603f6de75d1bc90acd1626c4c5f983ca2eb047c
Drashti Modasara <drashti@Drashtis-MacBook-Pro.local>
2024-04-24 13:17:49 +0530

    upload marquee poc

--- creativecloud/features/interactive-components/selector-tray/selector-tray.js
+++ creativecloud/features/interactive-components/selector-tray/selector-tray.js
@@ -2,6 +2,7 @@ import { createTag } from '../../../scripts/utils.js';
 import { handleImageTransition, getImgSrc } from '../../../blocks/interactive-metadata/interactive-metadata.js';
 
 function getTrayConfig(data) {
+  console.log(data);
   const dpth = data.displayPath;
   const allTrays = data.stepConfigs[data.stepIndex].querySelectorAll('ul, ol');
   const configTray = (dpth >= 0 && allTrays.length > dpth) ? allTrays[dpth] : allTrays[0];
@@ -69,15 +70,65 @@ function selectorTrayWithImgs(layer, data) {
   return selectorTray;
 }
 
+function selectorTrayWithSubMenu(layer, data) {
+  console.log('submenu');
+  const selectorTray = createTag('div', { class: 'body-s selector-tray change-bg' });
+  const trayItems = createTag('div', { class: 'tray-items' });
+  const configTray = getTrayConfig(data);
+  console.log(configTray);
+  const options = configTray.querySelectorAll('li');
+  let pathIdx = getStartingPathIdx(data);
+  let displayImg = null;
+  [...options].forEach((o) => {
+    const [thumbnailPic, displayPic] = o.querySelectorAll('picture');
+    const optionText = o.textContent.trim();
+    console.log(optionText);
+    if (displayPic) {
+      displayImg = [getImgSrc(displayPic), displayPic.querySelector('img').alt];
+      const a = createSelectorThumbnail(thumbnailPic, pathIdx, displayImg);
+      const trayOption = createTag('div', { class: 'tray-option' });
+      trayOption.append(a, optionText);
+      trayItems.append(trayOption);
+      pathIdx += 1;
+      attachThumbnailEvents(a, data, layer);
+    } else {
+      // const btnText = o.textContent
+      // we have optionText and thumbnail pic
+      const trayReset = createTag('div', { class: 'tray-reset' });
+      trayReset.append(thumbnailPic, optionText);
+      trayItems.append(trayReset);
+      // trayOption.append(a, optionText);
+      // pathIdx += 1;
+      // attachThumbnailEvents(a, data, layer);
+    }
+  });
+  selectorTray.append(trayItems);
+  return selectorTray;
+}
+
+function createSelectorTray(layer, data) {
+  // const selectorTray = createTag('div', { class: 'body-s selector-tray' });
+  // const trayItems = createTag('div', { class: 'tray-items' });
+  const configTray = getTrayConfig(data);
+  const options = configTray.querySelectorAll('li');
+  console.log(options);
+  if (options[0].textContent) {
+    console.log(options[0].textContent);
+    return selectorTrayWithSubMenu(layer, data);
+  }
+  return selectorTrayWithImgs(layer, data);
+}
+
 export default async function stepInit(data) {
   data.target.classList.add('step-selector-tray');
   const config = data.stepConfigs[data.stepIndex];
   const layer = createTag('div', { class: `layer layer-${data.stepIndex}` });
   const title = config.querySelector('p:first-child');
+  const hasPic = title.firstElementChild.tagName === 'PICTURE';
   let trayTitle = null;
-  if (title) trayTitle = createTag('div', { class: 'tray-title' }, title.innerText.trim());
-  const selectorTray = selectorTrayWithImgs(layer, data);
-  if (title) selectorTray.prepend(trayTitle);
+  if (title && !hasPic) trayTitle = createTag('div', { class: 'tray-title' }, title.innerText.trim());
+  const selectorTray = createSelectorTray(layer, data);
+  if (title && !hasPic) selectorTray.prepend(trayTitle);
   layer.append(selectorTray);
   return layer;
 }
